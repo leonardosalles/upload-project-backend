1a03366493b51c09356949a03ecedb08d5e60bcc
Leonardo Salles <leonardo_guirdo@hotmail.com>
2014-05-21 13:39:36 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-	email: 		{ type: String },
+	id: 		{ type: String },
 	password:	{ type: Number },
 	level: 		{ type: String, enum : ['admin', 'comum']},
 });
